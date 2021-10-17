e05a12d4a3db1f602c3263a8f879f064f1615188
oudahnayef <odah97884@gmail.com>
2021-10-17 16:59:13 +0300

    TP

---
+++ index.js
@@ -0,0 +1,8 @@
+const fruits = [{fruitName: "orang" ,url: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Oranges_-_whole-halved-segment.jpg/1200px-Oranges_-_whole-halved-segment.jpg" width="70px" height="70px"} ,
+
+{fruitName: "tomato"  ,url:"https://post.healthline.com/wp-content/uploads/2020/09/tomatoes-1200x628-facebook-1200x628.jpg"  },
+{fruitName: "bnana"  ,url:  "https://5.imimg.com/data5/EG/DZ/EI/SELLER-74811959/banana-500x500.jpg"            } ,
+{fruitName: "Strawberry"  ,url:  "https://modo3.com/thumbs/fit630x300/95912/1470832100/%D9%81%D9%88%D8%A7%D8%A6%D8%AF_%D8%A7%D9%84%D9%81%D8%B1%D8%A7%D9%88%D9%84%D8%A9_%D9%84%D9%84%D8%AC%D8%B3%D9%85.jpg"             }
+{fruitName: "watermelon"  ,url:  "https://alamphoto.com/wp-content/uploads/2017/08/Watermelon%20Images%20(2).jpg" },
+
+  ];
