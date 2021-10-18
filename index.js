c1919df9ed3772255e5b834f42b7674b4e3ad56c
oudahnayef <odah97884@gmail.com>
2021-10-18 17:03:04 +0300

    tp

--- index.js
+++ index.js
@@ -1,8 +1,50 @@
-const fruits = [{fruitName: "orang" ,url: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Oranges_-_whole-halved-segment.jpg/1200px-Oranges_-_whole-halved-segment.jpg" width="70px" height="70px"} ,
+const mainContainer  = $("#mainContainer");
+const nameInput = $("#name frot");
+const ageInput= $("#fruit price");
+const  fruitprice = $("#fruit age");
+// an array of students with some data
+const frotName  = [
+  {nameInput:"tomato ",ageInput: "1wek", fruitpriceLInput:"$2", URL: "https://media.istockphoto.com/photos/tomato-isolated-on-white-background-picture-id466175630?k=6&m=466175630&s=612x612&w=0&h=fu_mQBjGJZIliOWwCR0Vf2myRvKWyQDsymxEIi8tZ38="},
+  {nameInput:"banana ",ageInput: "1wek", fruitprice:"$1", URL: "https://cdn.shopify.com/s/files/1/2154/4937/products/banana_600x.jpg?v=1610541466"},
+  {nameInput:"orang ",ageInput: "1wek", fruitprice:"$4" , URL:"https://images.pexels.com/photos/42059/citrus-diet-food-fresh-42059.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" },
+  {nameInput:"abel",ageInput: "1wek", fruitprice:"$2" , URL:"https://pm1.narvii.com/7097/f3140869dea500182254dcd0181037a15724197cr1-600-600v2_hq.jpg" },
+];
 
-{fruitName: "tomato"  ,url:"https://post.healthline.com/wp-content/uploads/2020/09/tomatoes-1200x628-facebook-1200x628.jpg"  },
-{fruitName: "bnana"  ,url:  "https://5.imimg.com/data5/EG/DZ/EI/SELLER-74811959/banana-500x500.jpg"            } ,
-{fruitName: "Strawberry"  ,url:  "https://modo3.com/thumbs/fit630x300/95912/1470832100/%D9%81%D9%88%D8%A7%D8%A6%D8%AF_%D8%A7%D9%84%D9%81%D8%B1%D8%A7%D9%88%D9%84%D8%A9_%D9%84%D9%84%D8%AC%D8%B3%D9%85.jpg"             }
-{fruitName: "watermelon"  ,url:  "https://alamphoto.com/wp-content/uploads/2017/08/Watermelon%20Images%20(2).jpg" },
 
-  ];
+function renderArr(array) {
+  for (let i = 0; i < array.length; i++) {
+    const div = $(`<div class="frotclaas"'>
+        <h2>${array[i].nameInput}</h2>
+        <h2>${array[i].ageInput}</h2>
+        <h2>${array[i].fruitprice}</h2>
+    <img class="img" src=${array[i].URL} >
+        
+    </div>`);
+    mainContainer.append(div);
+  }
+}
+
+renderArr( frotName);
+
+function addfrot() {
+  
+  const newfrot = {
+    frotName: nameInput.val(),
+    frotAge: ageInput.val(),
+    fruitprice: fruitpriceLInput.val(),
+  };
+ 
+  frotName.push(newfrot);
+
+  renderArr([newfrot]);
+}
+
+function hideHandler() {
+ 
+  mainContainer.hide();
+}
+
+function showHandler() {
+  
+  mainContainer.show();
+}
